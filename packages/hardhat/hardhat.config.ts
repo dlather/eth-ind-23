72cdbbc6caf1fd05e5c99851642a4fa625eaedfc
dlather <deepaklather2709@gmail.com>
2023-12-10 01:33:46 +0530

    defaulr goerli network

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -31,7 +31,7 @@ const config: HardhatUserConfig = {
     },
   },
   // defaultNetwork: "baseGoerli",
-  defaultNetwork: "arbitrumGoerli",
+  defaultNetwork: "goerli",
   namedAccounts: {
     deployer: {
       // By default, it will take the first Hardhat account as the deployer
